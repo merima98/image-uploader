942d5ec2aecacfc920daac872309849990ee9431
Merima Ćeranić <55186271+merima98@users.noreply.github.com>
2022-04-29 13:26:49 +0200

    Registration setup. (#2)
    * Added login and registration components.
    
    * Implemented registration.

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 import Registration from "./components/registration/Registration";
 
@@ -6,11 +7,17 @@ const LOGGED_OUT_USER = [
     path: "/",
     element: Login,
   },
-
   {
     path: "/register",
     element: Registration,
   },
 ];
 
-export { LOGGED_OUT_USER };
+const LOGGED_IN_USER_ROUTES = [
+  {
+    path: "/",
+    element: Home,
+  },
+];
+
+export { LOGGED_OUT_USER, LOGGED_IN_USER_ROUTES };
