c12b232f5cc6cf90ae7c4b3a86d7aaefe54d7660
merima98 <merimaceranic98@gmail.com>
2022-05-05 14:58:14 +0200

    Removed App.css and added color mode into application

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,34 @@
-import { Breadcrumb, Box, BreadcrumbItem, Center } from "@chakra-ui/react";
+import {
+  Breadcrumb,
+  Box,
+  BreadcrumbItem,
+  Text,
+  Center,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Sun, Moon } from "react-feather";
 
 const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const backgroundColor = useColorModeValue("white", "gray.800");
+
   return (
     <Center mb={4}>
       <Breadcrumb
         p={4}
-        backgroundColor={"white"}
+        backgroundColor={backgroundColor}
         position={"fixed"}
         w={"100%"}
         top={"0"}
       >
         <BreadcrumbItem>
           <Box mb={2}>Home</Box>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <Text>
+            <Sun onClick={toggleColorMode} />
+          </Text>
         </BreadcrumbItem>
       </Breadcrumb>
     </Center>
